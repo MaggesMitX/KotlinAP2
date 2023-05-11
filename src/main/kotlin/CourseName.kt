5d46b5714ca4d9d933677614c9888daf039ed34e
Maximilian Ramacher <mail@max-ramacher.de>
2023-05-11 21:44:52 +0200

    remove für Aufgabe 2

--- src/main/kotlin/CourseName.kt
+++
@@ -1,6 +0,0 @@
-enum class CourseName {     // Enums können eine feste Anzahl von Konstanten haben
-    MATH,
-    CHEMISTRY,
-    ENGLISH;
-}
-        // Enums können auch Methoden haben, genau wie Klassen. Diese können auf die Konstanten des Enums zugreifen und sie manipulieren.
