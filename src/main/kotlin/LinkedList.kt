6adb6f1d75e3d0f68fb1224f2161fcf113125e84
Maximilian Ramacher <mail@max-ramacher.de>
2023-06-08 13:38:54 +0200

    release Praktikum 3

--- src/main/kotlin/LinkedList.kt
+++ src/main/kotlin/LinkedList.kt
@@ -3,27 +3,103 @@ class LinkedList <T: Comparable <T>> {
 
     private var first: Node<T>? = null
 
-    fun isEmpty() {
-        TODO()
+    var countForSize : Int = 0
+
+    // => Gibt zurück, ob die Liste leer ist oder nicht.
+    fun isEmpty() : Boolean = first == null
+
+    fun addFirst(data: T) { // => Fügt data als neuen Knoten (Node) am Anfang der Liste hinzu
+        val runPointer =  Node(data, null)
+        runPointer.next = first
+        first = runPointer
+        countForSize++
     }
 
-    fun addFirst(data: T) {
-        TODO()}
+    fun clear() {// => Leert die Liste.
+        first = null
+    }
 
-    fun clear() {
-        TODO()}
+/*    fun size() : Int { // => Gibt zurück, wie viele Elemente die Liste hat.
+        var count = 0
+        var runPointer = first
+        while (runPointer != null) {
+            count ++
+            runPointer = runPointer.next
+        }
+        return count
+    }*/
 
-    fun size() {
-        TODO()}
+    override fun toString(): String { // => Wandelt die verkettete Liste in einen String um und gibt diesen zurück.
+        val element = mutableListOf<T>()
+        var runPointer = first
+        while (runPointer != null) {
+            element.add(runPointer.data)
+            runPointer = runPointer.next
+        }
+        return element.joinToString(prefix = "[", postfix = "]")
+    }
 
-    fun toString() {
-        TODO()}
+    fun addLast(data: T) { // => Fügt data als neuen Knoten am Ende der Liste hinzu.
+        var runPointer = Node(data, null)
+        if (isEmpty()) {
+            first = runPointer
+        } else {
+            var newRunPointer = first
+            while (newRunPointer?.next != null) {
+                newRunPointer = newRunPointer.next
+            }
+            newRunPointer?.next = runPointer
+            countForSize++
+        }
+    }
 
-    fun addLast(data: T) {
-        TODO()}
+    fun addSorted(data: T) { // => Fügt data als neuen Knoten an der richtigen Stelle (sortiert) hinzu.
+        var runPointer = Node(data, null)
+        if (isEmpty() || data < first!!.data) {
+            runPointer.next = first
+            first = runPointer
+        } else {
+            var newRunPointer = first
+            while (newRunPointer?.next != null && newRunPointer.next!!.data < data) {
+                newRunPointer = newRunPointer.next
+            }
+            runPointer.next = newRunPointer?.next
+            newRunPointer?.next = runPointer
+            countForSize++
+        }
 
-    fun addSorted(data: T) {
-        TODO()}
-    fun appendList(NewList: LinkedList<T>) {
-        TODO()}
+
+        }
+    fun appendList(newList: LinkedList<T>) { // => Fügt die übergebene Liste ans Ende der aktuellen Liste hinzu.
+        if (isEmpty()) {
+            first = newList.first
+        } else {
+            var runPointer =first
+            while (runPointer?.next != null) {
+                runPointer = runPointer.next
+            }
+        }
+    }
+    fun forEach(action: (T)-> Unit) {
+        var runPointer = first
+        while (runPointer != null) {
+            action(runPointer.data)
+            runPointer = runPointer.next
+        }
+    }
+    fun size() : Int {
+        println("Hier wird countForSize ausgegeben")
+        return countForSize
+    }
+    fun firstWhere(condition: (T)-> Boolean): T? {
+        var runPointer = first
+        while (runPointer != null) {
+            if (condition(runPointer.data)) {
+                return runPointer.data
+            }
+            runPointer = runPointer.next
+        }
+        return null
+    }
+
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,46 @@
 fun main () {
-    val fruits = LinkedList < String >()
-    fruits . addFirst (" Birne ")
-    fruits . addFirst (" Apfel ")
-    println ( fruits . toString () ) // [Apfel , Birne ]
-    fruits . addLast (" Erdbeere ")
-    println ( fruits . toString () ) // [Apfel , Birne , Erdbeere ]
-    fruits . addSorted (" Banane ")
-    println ( fruits . toString () ) // [Apfel , Banane , Birne , Erdbeere ]
-    fruits . addSorted (" Blaubeere ")
-    println ( fruits . toString () ) // [Apfel , Banane , Birne , Blaubeere ,
-    Erdbeere ]
-    val newList = LinkedList <String >()
-    newList . addLast (" Wassermelone ")
-    newList . addLast (" Mango ")
-    fruits . appendList ( newList )
-    println ( fruits . toString () ) // [Apfel , Banane , Birne , Blaubeere ,
-    Erdbeere , Wassermelone , Mango ]
+    val fruits = LinkedList<String>()
+    val newList = LinkedList<String>()
+    fruits.addFirst("Birne")
+    fruits.addFirst("Apfel")
+    println(fruits.toString()) // [Apfel, Birne]
+    fruits.addLast("Erdbeere")
+    println(fruits.toString()) // [Apfel, Birne, Erdbeere]
+    fruits.addSorted("Banane")
+    println(fruits.toString()) // [Apfel, Banane, Birne, Erdbeere]
+    fruits.addSorted("Blaubeere")
+    println(fruits.toString()) // [Apfel, Banane, Birne, Blaubeere, Erdbeere]
+
+    println("Jetzt gleich kommt CountForSize")
+    println(fruits.size())
+
+
+    newList.addLast("Wassermelone")
+    newList.addLast("Mango")
+    fruits.appendList(newList)
+    println(fruits.toString()) // [Apfel, Banane, Birne, Blaubeere, Erdbeere, Wassermelone, Mango]
+
+    fruits.forEach { element ->
+        println(element)
+    }
+
+    val found = fruits . firstWhere { element ->
+        element . startsWith ("B")
+    }
+    println ( found ) // Banane
+    val found2 = fruits . firstWhere { element ->
+        element . contains (" beere ")
+    }
+    println ( found2 ) // Blaubeere
+    val found3 = fruits . firstWhere { element ->
+        element . length > 10
+    }
+    println ( found3 ) // Wassermelone
+
 }
+//Aufgabe 2 Was sind Funktionen höherer Ordnung?
+
+// Was bedeutet es, wenn ein Algorithmus eine Laufzeit von O(1), O(n) oder O(n2) hat?
+// O(1) -> Laufzeit bleibt gleich egal wie groß die Eingabe ist. Immer gleiche Zugriffszeit Bsp. Algorithmus= variable deklarieren/ If abfrage
+// O(n) ->  Lineare Laufzeit, die proportional zur Größe der Eingabe ist. Bsp. Algorithmus= while schleife(input menge)
+// O(n^2) -> Quadratische Laufzeit, die exponentiell der Eingabe steigt. Bsp. Algorithmus= Zwei ineinandergeschachtelte schleifen
